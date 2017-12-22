c52692eb77c41fd77b311b6210ae58be08fa9113
mario <mario.saliba98@gmail.com>
2017-12-22 22:33:41 +0200

    handle empty input

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 #!/usr/bin/env node
 
+if (!process.argv[2]) {
+  console.log(
+    'Please input what would you like youre project to be name after the command'
+  );
+  return;
+}
+
 require('./ms.js');
 
 console.log('Fetching files ..');
