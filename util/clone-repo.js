392b4daabbd60f1a0014968b6e103aced31b9a7d
mario <mario.saliba98@gmail.com>
2017-12-26 19:50:01 +0200

    fix my dummy error

--- util/clone-repo.js
+++ util/clone-repo.js
@@ -1,4 +1,4 @@
-const { spawn } = require('child_process');
+const { spawn, exec } = require('child_process');
 
 exports.clone = (repo, targetPath, cb) => {
   const process = spawn('git', ['clone', repo, targetPath]);
@@ -13,18 +13,16 @@ exports.clone = (repo, targetPath, cb) => {
 };
 
 exports.removeRepo = (repo, targetPath, cb) => {
-  const process = spawn('git', ['status']);
-
-  process.on('close', status => {
-    if (status === 0) {
-      cb && cb();
-    } else {
+  exec(`rm -rf ${targetPath}/.git`, (err, stdout, stderr) => {
+    if (err) {
       cb &&
         cb(
           new Error(
             'Something went wrong please try again. if you problem was not resolved please raise an issue on the matter https://github.com/Karyum/Create-Karyum-App-npm'
           )
         );
+    } else {
+      cb && cb();
     }
   });
 };
